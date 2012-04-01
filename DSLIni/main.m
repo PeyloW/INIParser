8cfa572aefdac585559b4d42f822bd69bed43026
Fredrik Olsson <peylow@gmail.com>
2012-04-01 18:51:02 +0200

    Initial Commit

---
+++ DSLIni/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DSLIni
+//
+//  Created by Fredrik Olsson on 2012-04-01.
+//  Copyright (c) 2012 Jayway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
