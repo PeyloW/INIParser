2ada58f17383a16ab1afcc3d7456f496efa1c3b3
Fredrik Olsson <peylow@gmail.com>
2012-04-02 22:39:23 +0200

    Initial import

---
+++ DSLIni/PEWINIParser.h
@@ -0,0 +1,13 @@
+//
+//  PWINIParser.h
+//  DSLIni
+//
+//  Created by Fredrik Olsson on 2012-04-01.
+//  Copyright (c) 2012 Jayway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PWINIParser : NSObject
+
+@end
---
+++ DSLIni/PEWINIParser.m
@@ -0,0 +1,13 @@
+//
+//  PWINIParser.m
+//  DSLIni
+//
+//  Created by Fredrik Olsson on 2012-04-01.
+//  Copyright (c) 2012 Jayway. All rights reserved.
+//
+
+#import "PWINIParser.h"
+
+@implementation PWINIParser
+
+@end
